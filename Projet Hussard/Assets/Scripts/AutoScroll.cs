7ff6c22ef2e6d1998f6aee2dfe3e9ddcaa52e550
JeremyForgues <59878648+JeremyForgues@users.noreply.github.com>
2020-01-29 12:08:34 +0100

    jeremy

--- Projet Hussard/Assets/Scripts/AutoScroll.cs
+++ Projet Hussard/Assets/Scripts/AutoScroll.cs
@@ -4,13 +4,40 @@ using UnityEngine;
 
 public class AutoScroll : MonoBehaviour
 {
+    private Vector3 offset;
+    [SerializeField] private Transform target;
     public float timeScroll = 30f;
+    public bool isOnScroll;
 
-    void OnTriggerStay2D(Collider2D collision)
+    private void Start()
+    {
+        offset = transform.position - target.transform.position;
+    }
+
+    private void Update()
+    {
+        transform.position = target.transform.position + offset;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
+            if (isOnScroll)
+            {
 
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (!isOnScroll)
+            {
+
+            }
         }
     }
 
